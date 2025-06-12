c9dabf3fa3a94b2aab52d63cd7093d56a25a47a3
shubh72010 <108581630+shubh72010@users.noreply.github.com>
2025-06-12 13:47:47 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,18 @@
-const CACHE_NAME = 'scaledspace-v1';
-const toCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
-  // Add other files if you want (e.g., CSS, JS, images)
-];
-
-// Cache app shell during install
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(toCache))
-  );
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
 });
-
-// Serve cached content when offline
-self.addEventListener('fetch', event => {
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
-  );
-});
-
-// Optional: Clean up old caches (advanced, safe to omit)
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      )
-    )
+    caches.open('v1').then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
   );
 });
