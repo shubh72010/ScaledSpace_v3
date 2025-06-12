b16ca0047f500136da7f642bdf4c13988fe12ada
shubh72010 <108581630+shubh72010@users.noreply.github.com>
2025-06-12 14:00:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css"; // If you do not have index.css, remove this line!
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
